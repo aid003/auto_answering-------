0cafa4a6d3ef57cf021b623b4621ce27f6728c23
aid003 <teremochek859@gmail.com>
2024-12-04 22:43:22 +0300

    deweweewdqwdwqd

--- avitoHandlers/bot.js
+++ avitoHandlers/bot.js
@@ -64,6 +64,14 @@ export async function botAvito() {
       const message = await chekIdAdvForAnswer(chat.context.value.id);
 
       await sendMessage(process.env.USER_ID, chat.id, currentToken, message);
+      await sleep(2000);
+
+      await sendMessage(
+        process.env.USER_ID,
+        chat.id,
+        currentToken,
+        "https://t.me/GMTUSDT"
+      );
 
       let messageStr = "🟢 Новый клиент";
       messageStr += `\n\nОбъявление: ${chat.context.value.title}`;
